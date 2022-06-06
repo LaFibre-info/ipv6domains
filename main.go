1f588178d252c15757300b226f179d59a2753ff9
kgersen <kgersen@hotmail.com>
2022-06-07 00:11:51 +0200

    - using embed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"html/template"
 	"log"
@@ -10,8 +11,10 @@ import (
 	"os"
 	"sort"
 	"strings"
-	"unicode/utf8"
 )
+
+//go:embed results.html
+var results_tpl string
 
 type Result struct {
 	Domain   string
@@ -24,61 +27,6 @@ type Result struct {
 	MX6      []string
 	WWW4     []string
 	WWW6     []string
-}
-
-// from https://gist.github.com/chmike/d4126a3247a6d9a70922fc0e8b4f4013
-// checkDomain returns an error if the domain name is not valid
-// See https://tools.ietf.org/html/rfc1034#section-3.5 and
-// https://tools.ietf.org/html/rfc1123#section-2.
-func checkDomain(name string) error {
-	switch {
-	case len(name) == 0:
-		return fmt.Errorf("check domain: empty domain")
-	case len(name) > 255:
-		return fmt.Errorf("check domain: name length is %d, can't exceed 255", len(name))
-	}
-	var l int
-	for i := 0; i < len(name); i++ {
-		b := name[i]
-		if b == '.' {
-			// check domain labels validity
-			switch {
-			case i == l:
-				return fmt.Errorf("check domain: invalid character '%c' at offset %d: label can't begin with a period", b, i)
-			case i-l > 63:
-				return fmt.Errorf("check domain: byte length of label '%s' is %d, can't exceed 63", name[l:i], i-l)
-			case name[l] == '-':
-				return fmt.Errorf("check domain: label '%s' at offset %d begins with a hyphen", name[l:i], l)
-			case name[i-1] == '-':
-				return fmt.Errorf("check domain: label '%s' at offset %d ends with a hyphen", name[l:i], l)
-			}
-			l = i + 1
-			continue
-		}
-		// test label character validity, note: tests are ordered by decreasing validity frequency
-		if !(b >= 'a' && b <= 'z' || b >= '0' && b <= '9' || b == '-' || b >= 'A' && b <= 'Z') {
-			// show the printable unicode character starting at byte offset i
-			c, _ := utf8.DecodeRuneInString(name[i:])
-			if c == utf8.RuneError {
-				return fmt.Errorf("check domain: invalid rune at offset %d", i)
-			}
-			return fmt.Errorf("check domain: invalid character '%c' at offset %d", c, i)
-		}
-	}
-	// check top level domain validity
-	switch {
-	case l == len(name):
-		return fmt.Errorf("check domain: missing top level domain, domain can't end with a period")
-	case len(name)-l > 63:
-		return fmt.Errorf("check domain: byte length of top level domain '%s' is %d, can't exceed 63", name[l:], len(name)-l)
-	case name[l] == '-':
-		return fmt.Errorf("check domain: top level domain '%s' at offset %d begins with a hyphen", name[l:], l)
-	case name[len(name)-1] == '-':
-		return fmt.Errorf("check domain: top level domain '%s' at offset %d ends with a hyphen", name[l:], l)
-	case name[l] >= '0' && name[l] <= '9':
-		return fmt.Errorf("check domain: top level domain '%s' at offset %d begins with a digit", name[l:], l)
-	}
-	return nil
 }
 
 // QueryHost performs net.LookupHost on a host name and return the responses in distinct IPv4 and IPv6 lists.
@@ -199,7 +147,7 @@ func main() {
 }
 
 func server() {
-	tpl, err := template.New("page").ParseFiles("./results.html")
+	tpl, err := template.New("page").Parse(results_tpl)
 	if err != nil {
 		log.Fatal(err)
 	}
